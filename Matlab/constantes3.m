ef741ec7c82091a1ad77f3fda87e7a399ac8f343
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-09-30 12:10:38 +0200

    Td 3 fini
    Début projet

--- Matlab/constantes3.m
+++ Matlab/constantes3.m
@@ -1,8 +1,10 @@
 clear; close all; clc;
 
 R = 0.895;     %?
-L = 12.1610-3; %H
+L = 12.16e-3; %H
 J = 0.2;       %kg.m2.s?2
 K = 0.9;       %V.s.rad?1
-P = 3;
-f = K/p;
+p = 3;         % Nombre de paires de pôles
+Phi = K/p;       %Constante électromécanique
+psi=pi/4;
+U0=220;     
---
+++ Matlab/constantes_ABS.m
@@ -0,0 +1,11 @@
+ear; close all; clc;
+
+g=9.81; 
+v0=27.77; %vitesse initiale de la voiture
+rRoue=0.3; %rayon de la roue
+J=0.5;% moment inertie roue
+m=1000;%masse voiture
+
+%Cas du freinage sans glissement : F<= fN avec f=0.8
+%Cas du freinage avec glissement : F=f(g)N
+
